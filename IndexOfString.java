434df7745e50dbecd6e2ec0d259cf7c07c00eeb2
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-08 14:02:31 -0300

    indexof string

---
+++ IndexOfString.java
@@ -0,0 +1,8 @@
+public class IndexOfString {
+    public static void main(String[]args) {
+        String text = "Testando Tudo e Todos";
+        int indexOfText = text.indexOf("Tudo");
+        System.out.println("Tudo está em " + indexOfText);
+
+    }
+}
