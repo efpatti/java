5537c04eafbcb13f6e1503f108db51bee1d68bd7
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-08 12:24:52 -0300

    area triangulo em java

--- Constant.java
+++ Constant.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Constant {
     public static void main(String[]args) {
-        final double pi = 3.14;
+        final float pi = 3.14f;
+        double base, height, area_triangulo;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base: ");
+        base = sc.nextDouble();
+        System.out.print("Altura: ");
+        height = sc.nextDouble();
+        area_triangulo =  (base * height) / 2;
         System.out.println("Isto é uma constante: " + pi);
+        System.out.println("A área do triangulo é: " + area_triangulo);
+        sc.close();
     }
 }
