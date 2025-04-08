a3d7a0fbc01ce8d36a0d3d4de97d42d0b19b924b
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-08 14:12:34 -0300

    string sum

---
+++ StringSum.java
@@ -0,0 +1,9 @@
+public class StringSum {
+    public static void main(String[]args) {
+        String n1, n2, sum;
+        n1 = "10";
+        n2 = "20";
+        sum = n1 + n2;
+        System.out.println(sum);
+    } 
+}
