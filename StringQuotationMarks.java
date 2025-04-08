4e898b938feb24466b7b1013bb7340fe32eded67
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-08 14:22:37 -0300

    line break, backslash, etc

--- StringQuotationMarks.java
+++ StringQuotationMarks.java
@@ -2,5 +2,11 @@ public class StringQuotationMarks {
     public static void main(String[]args) {
         String text = "I am super \"Cool!\"";
         System.out.println(text);
+        String txt1 = "It\'s alright.";
+        System.out.println(txt1);
+        String txt2 = "Backslash \\.";
+        System.out.println(txt2);
+        String txt3 = "Line \nBreak!";
+        System.out.println(txt3);
     }
 }
