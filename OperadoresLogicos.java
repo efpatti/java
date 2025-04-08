2b7900267d8eaf4b277b848d6608c9e5fc9edba0
Enzo Patti <enzo.patti@aluno.senai.br>
2025-04-08 12:56:37 -0300

    operadores logicos

---
+++ OperadoresLogicos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class OperadoresLogicos {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        boolean diferente;
+        int n1, n2;
+        System.out.print("Número 1: ");
+        n1 = sc.nextInt();
+        System.out.print("Número 2: ");
+        n2 = sc.nextInt();
+        diferente = n1 != n2 ? true : false;
+        sc.close();
+        if (diferente) {
+            if(n1 > n2) {
+                System.out.println("O número 1 é maior!");
+            } 
+            else {
+                System.out.println("O número 2 é maior!");
+            }
+        }
+        else {
+            System.out.println("Os números são iguais!");
+        }
+    }
+}
